26f7861f4ebe0eb71607dc3cad7e19503bfc44b3
KJI-DEC <kji_dec22@ewhain.net>
2021-12-30 17:46:56 +0900

    initial commit

---
+++ src/test/java/com/example/adobbyspringboot/AdobbySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adobbyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdobbySpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
