ebb8318d7bdd5a98f52fec18b151b339ffd256f6
KJI-DEC <kji_dec22@ewhain.net>
2022-01-01 18:43:28 +0900

    [Feat] findByAndroidId 추가

--- src/main/java/com/example/adobbyspringboot/domain/user/UserRepository.java
+++ src/main/java/com/example/adobbyspringboot/domain/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.example.adobbyspringboot.domain.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, Long> {
+    Optional<User> findByAndroidId(String androidId);
 }
