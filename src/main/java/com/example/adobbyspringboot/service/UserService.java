f53b662ebb7cb717de00af6f5e72020b901601d1
KJI-DEC <kji_dec22@ewhain.net>
2022-01-02 22:29:55 +0900

    [Feat] getUserDiary, userDiaryToResponse 추가

--- src/main/java/com/example/adobbyspringboot/service/UserService.java
+++ src/main/java/com/example/adobbyspringboot/service/UserService.java
@@ -3,9 +3,13 @@ package com.example.adobbyspringboot.service;
 import com.example.adobbyspringboot.domain.diary.Diary;
 import com.example.adobbyspringboot.domain.user.User;
 import com.example.adobbyspringboot.domain.user.UserRepository;
+import com.example.adobbyspringboot.payload.response.UserListResponse;
+import com.example.adobbyspringboot.payload.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,7 +25,31 @@ public class UserService {
         );
     }
 
-    public List<Diary> getUserDiary(String androidId){
-        return findUserByAndroidId(androidId).getDiaries();
+    public List<Diary> getUserDiary(String androidId, Long lastDiaryId, int size){
+        int lastIndex = 0;
+        List<Diary> diaries = findUserByAndroidId(androidId).getDiaries();
+        if(diaries == null || diaries.isEmpty()){
+            Diary diary = new Diary(0L, LocalDate.now(), "","");
+            List<Diary> emptyDiary = new ArrayList<>();
+            emptyDiary.add(diary);
+            return emptyDiary;
+        }
+        if(diaries.size() < lastDiaryId.intValue() + size)
+            lastIndex = diaries.size();
+        else
+            lastIndex = lastDiaryId.intValue() + size;
+        return diaries.subList(lastDiaryId.intValue(), lastIndex);
+    }
+
+    public UserListResponse userDiaryToResponse(String androidId, Long lastDiaryId, int size){
+        List<Diary> diaries = getUserDiary(androidId, lastDiaryId, size);
+        List<UserResponse> userResponses = new ArrayList<>();
+
+        for(Diary diary : diaries){
+            UserResponse userResponse = new UserResponse(diary.getId(), diary.getLine());
+            userResponses.add(userResponse);
+        }
+
+        return new UserListResponse(userResponses);
     }
 }
