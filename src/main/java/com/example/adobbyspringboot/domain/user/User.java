d09f62fc644527d9e4a4c2983273345c74cd84bd
KJI-DEC <kji_dec22@ewhain.net>
2022-01-02 21:52:19 +0900

    [Fix] 오타 수정(collection), setter 삭제

--- src/main/java/com/example/adobbyspringboot/domain/user/User.java
+++ src/main/java/com/example/adobbyspringboot/domain/user/User.java
@@ -4,16 +4,14 @@ import com.example.adobbyspringboot.domain.diary.Diary;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 @Getter
-@Setter
 @Builder(access = AccessLevel.PUBLIC)
-@Document(collation = "user")
+@Document(collection = "user")
 public class User {
     @Id
     private String androidId;
